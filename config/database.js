5a72ef173eb1b0d6a4f21e670606f34ae21a8c92
gocrushgoals <gocrushgoals@gmail.com>
2024-05-16 10:42:58 -0400

    update handlebars

--- config/database.js
+++ config/database.js
@@ -4,8 +4,5 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize('blog_db', 'username', 'password', {
   dialect: 'mysql2',
   host: 'localhost',
-  port: 3306, // Optional: Specify the port number
-  logging: false, // Optional: Disable logging
-  timezone: '+00:00', // Optional: Specify the time zone
-  // Other options...
+  port: 3301, 
 });
--- server.js
+++ server.js
@@ -5,10 +5,15 @@ const port = 3001
 
 const app = express();
 
+
+app.use(express.static('public'));
+app.enable('view cache');
+
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
+//restful routes
 app.get('/', (req, res) => {
   res.render('home', {
     posts: ["Post 1", "Post 2", "Post 3", "Post 4"], 
@@ -21,6 +26,17 @@ app.get('/login', (req, res) => {
 });
 
 
+// Define a route for the about page
+app.get('/about', (req, res) => {
+    res.send('About Us');
+});
+
+// Define a route for the contact page
+app.get('/contact', (req, res) => {
+    res.send('Contact Us');
+});
+
+// Start the server
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
