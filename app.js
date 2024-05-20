1554c2b1717979dc65ecffd77edd432c7d90d440
gocrushgoals <gocrushgoals@gmail.com>
2024-05-20 14:30:44 -0400

    fixed app connection

--- app.js
+++ app.js
@@ -12,18 +12,10 @@ const SequelizeStore = require("connect-session-sequelize")(session.Store);
 // create database
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: 'mysql'
+  dialect: 'mysql',
+  dialectModule: require('mysql2'),
+  port: process.env.DB_PORT
 });
-
-
-// comment out  mysql for postgres
-// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-//   host: process.env.DB_HOST,
-//   dialect: 'mysql',
-//   dialectModule: require('mysql2'),
-//   port: process.env.DB_PORT
-// });
 
 const connection = require('./config/connection');
 const BlogPost = require('./models/BlogPost');
--- config/connection.js
+++ config/connection.js
@@ -3,10 +3,15 @@ require('dotenv').config();
 const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',      
-      dialect: 'mysql',      
+  ? new Sequelize(process.env.JAWSDB_URL, {
+      dialect: 'mysql',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    })
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      host: 'localhost',
+      dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
       },
